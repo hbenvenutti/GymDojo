c3b8c7f32ae526883e37ca6893791c92ac8f852a
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-08-15 23:54:22 -0300

    implementado o entity framework

---
+++ Infra/Database/APIContext.cs
@@ -0,0 +1,19 @@
+using GymAPI.Infra.Database.Config;
+using GymAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymAPI.Infra.Database;
+
+public class APIContext : DbContext
+{
+    public DbSet<Student> Students { get; set; }
+
+    // ---------------------------------------------------------------------- //
+
+    public APIContext(DbContextOptions<APIContext> options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new StudentDBConfig());
+    }
+}
